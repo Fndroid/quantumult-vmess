fbf9a923e676cc82677d52e4a1c15d2255e6fecd
Fndroid <18825176954@163.com>
2019-08-21 15:42:40 +0800

    Updates

---
+++ precache-manifest.d0fcc80a24b5049a34adbf7e9011a9b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd797c30490b16697cccb7b02c2aab72",
+    "url": "/quantumult-vmess/index.html"
+  },
+  {
+    "revision": "9f2e8eea4e796a6d184b",
+    "url": "/quantumult-vmess/static/css/main.77c5c58a.chunk.css"
+  },
+  {
+    "revision": "9249200a069e4e3bbf1a",
+    "url": "/quantumult-vmess/static/js/2.32d2ad9a.chunk.js"
+  },
+  {
+    "revision": "9f2e8eea4e796a6d184b",
+    "url": "/quantumult-vmess/static/js/main.ead456f9.chunk.js"
+  },
+  {
+    "revision": "25f1b13868d74736cc5e",
+    "url": "/quantumult-vmess/static/js/runtime~main.e22ab34e.js"
+  }
+]);
